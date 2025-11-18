db317d98bce55d89b5a707c3da8caa90b3c2e61b
marlonvt <marlon_kin@hotmail.com>
2025-11-18 20:30:56 -0300

    Aulas REACT

---
+++ cursos/react/curso_01/2_CURSO_E_PROJETO/cursoreact/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello React!</h2>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
+
+
+//PAREM em 20:48 da aula https://compassuol.udemy.com/course/react-fundamental-crie-o-seu-primeiro-projeto/learn/lecture/31781644#notes
---
+++ cursos/react/curso_01/2_CURSO_E_PROJETO/cursoreact/src/components/FirstComponent.js
@@ -0,0 +1,9 @@
+function FirstComponent() {
+    return (
+        <div>
+            <p>Primeiro component</p>
+        </div>
+    )
+}
+
+export default FirstComponent;
