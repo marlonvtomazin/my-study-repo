34083cd633f98d058a1c4bc9590329215255f161
marlonvt <marlon_kin@hotmail.com>
2025-12-02 18:34:45 -0300

    todo list update

--- cursos/react/curso_01/2_CURSO_E_PROJETO/todo-list/src/App.js
+++ cursos/react/curso_01/2_CURSO_E_PROJETO/todo-list/src/App.js
@@ -11,16 +11,55 @@ function App() {
   // Para loading da aplicação
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    }
+    //Envio para a api
+    console.log(todo);
+
+    setTitle("");
+    setTime("");
+  }
+
   return (
     <div className="App">
       <div className="todo-header">
         <h1>React Todo list</h1>
       </div>
       <div className="form-todo">
-        <p>Formulário</p>
+        <h2>Insira sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input type="text"
+              name="title"
+              placeholder="Título da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input type="text"
+              name="time"
+              placeholder="Tempo estimado (em horas)"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+          <input type="submit" value="Criar tarefa" />
+        </form>
       </div>
       <div className="list-todo">
-        <p>Lista</p>
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
       </div>
     </div>
   );
